6bf4434d0ce5f2cd9b7eea0788652f49cbb09433
worashf <uppert83@gmail.com>
2022-08-19 12:54:39 +0300

    Remove aditional method and linters error

--- module.rb
+++ module.rb
@@ -1,7 +1,7 @@
 module MyEnumerable
   def list
     @list ||= []
-  end 
+  end
 
   def all?
     ary = []
@@ -25,37 +25,4 @@ module MyEnumerable
     end
     p ary
   end
-
-  def max
-    max_num = 0
-    list.max do |item|
-      yield item
-    end
-    p max_num
-  end
-
-  def min
-    min_num = 9999  # Arbitrary large number
-    list.min do |item|
-      item < min_num ? min_num = item : min_num
-    end
-    p min_num
-  end
-
-  def sort
-    ary = []
-    new_ary = []
-    list.length.times do |i|
-      source_ary = list
-      ary << source_ary.delete_at(i)
-    end
-    ary.sort do |item|
-      yield item
-    end
-    ary.each do |item|
-      new_ary << item
-    end
-    p new_ary
-  end
-
 end

340364284b427fd8174ca80b5b7c1a8d4d0709a8
worashf <uppert83@gmail.com>
2022-08-19 12:52:19 +0300

    Remove linters error

---
+++ my_list.rb
@@ -0,0 +1,28 @@
+require_relative 'module'
+
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+#=> #<MyList: @list=[1, 2, 3, 4]>
+
+# Test #all?
+list.all? { |e| e < 5 }
+#=> true
+list.all? { |e| e > 5 }
+#=> false
+
+# Test #any?
+list.any?(&:zero?)
+#=> true
+list.any? { |e| e == 5 }
+#=> false
+
+# Test #filter
+list.filter(&:even?)
+#=> [2, 4]
